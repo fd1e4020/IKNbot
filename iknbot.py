b64419628b713b47bf6c4a10ca96d157c5f34768
divby0 <divby0@paperweight.local>
2021-03-30 09:20:45 -0500

    v0.2:
    Added %item and %itemlist commands

--- iknbot.py
+++ iknbot.py
@@ -8,6 +8,20 @@ import requests
 import urllib.parse
 from lxml import etree
 import sys
+import yaml
+
+# TODO for future upgrades, use .config.yaml instead
+# in particular:
+#
+#  groups:
+#    - GROUP_NAME_1:
+#      alt_name: ALT_NAME
+#      alt_pass: ALT_PASS
+#      group_color: COLOR_NUMBER for group
+#      auth_role: AUTHORIZED role
+#      auth_users:
+#        - user1
+#        - ...
 
 load_dotenv()
 TOKEN   = os.getenv('DISCORD_TOKEN')
@@ -28,11 +42,29 @@ url = 'http://urbandead.com/map.cgi?username=' + ALTNAME + '&password=' + ALTPAS
 try:
 	session = requests.session()
 	r = session.get(url)
-except requests.exceptions.RequestException as e:
+except Exception as e:
     raise SystemExit(e)
-if r.status_code != 200:
-	errmsg = 'failed to establish session, status ' + str(r.status_code)
-	sys.exit(errmsg)
+#if r.status_code != 200:
+#	errmsg = 'failed to establish session, status ' + str(r.status_code)
+#	sys.exit(errmsg)
+
+
+with open('items.yaml', 'r') as f:
+	try:
+		items = yaml.load(f, Loader=yaml.FullLoader)
+	except Exception as e:
+		sys.exit(e)
+	finally:
+		f.close()
+
+with open('item-abbrev.yaml', 'r') as f:
+	try:
+		abbrevs = yaml.load(f, Loader=yaml.FullLoader)
+	except Exception as e:
+		sys.exit(e)
+	finally:
+		f.close()
+
 
 bot = commands.Bot(command_prefix=PREFIX)
 
@@ -55,6 +87,9 @@ async def cmd_ping(ctx):
 # TODO DRY
 # TODO "paginate" if output is too long
 # TODO check for empty contact list
+
+# TODO use predicate function to authenticate from DM
+# TODO once multiple groups are supported, do not default group when DMd
 
 @bot.command(name='active', help='active group members')
 @commands.has_role(ALLOWED)
@@ -189,6 +224,50 @@ async def cmd_group(ctx):
 
 	await ctx.send(embed=embed)
 
+@bot.command(name='item', help='item quick-info')
+async def cmd_item(ctx, *args):
+	# don't require quoting
+	arg = ' '.join(args)
 
+	fields = [ "locations", "encumbrance", "accuracy", "damage", "notes"]
+
+	try:
+		arg = abbrevs[arg]
+	except:
+		pass
+
+	try:
+		item = items[arg]
+	except:
+		await ctx.send(arg + ' not found')
+		return
+
+	embed  =discord.Embed(
+		title=arg,
+		url=item['wiki'],
+		description="Here's what I know about " + arg + ":",
+		color=0xFF5733)
+
+	for f in fields:
+		try:
+			val = item[f]
+			embed.add_field(name=f, value=val, inline=False)
+		except:
+			pass
+
+	await ctx.send(embed=embed)
+
+@bot.command(name='itemlist', help='items I Know Nothing about')
+async def cmd_itemlist(ctx, *args):
+
+	ikn = '```\nknown items:\n\n'
+	for i in items:
+		ikn += i + '\n'
+	ikn += '\nabbreviatons:\n\n'
+	for k, v in abbrevs.items():
+		ikn += k + ': ' + v + '\n'
+	ikn += '```'
+
+	await ctx.send(ikn)
 
 bot.run(TOKEN)
